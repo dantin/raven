b3e4cea5bdb6f6f7435461191682d48eb53b43db
dantin <chengjie.ding@gmail.com>
2021-04-14 15:55:44 +0800

    rename biz class and  add enums.

--- raven/models.py
+++ raven/models.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
+import enum
 
 from flask_appbuilder import Model
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Enum, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+
+
+class VideoBoxTypes(str, enum.Enum):
+    xgc_500 = 'XGC 500'
+
+
+class VideoStreamTypes(str, enum.Enum):
+    camera = 'Camera'
+    device = 'Device'
 
 
 class Room(Model):
@@ -14,17 +24,22 @@ class Room(Model):
         return self.name
 
 
-class Device(Model):
+class VideoBox(Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
     serial_no = Column(String(255), nullable=False)
+    video_box_type = Column(Enum(VideoBoxTypes))
     room_id = Column(Integer, ForeignKey('room.id'))
     room = relationship('Room')
 
+    def __repr__(self):
+        return self.serial_no
 
-class Stream(Model):
+
+class VideoStream(Model):
     id = Column(Integer, primary_key=True)
     push_url = Column(String(255), nullable=False)
     broadcast_url = Column(String(255), nullable=False)
-    device_id = Column(Integer, ForeignKey('device.id'))
-    device = relationship('Device')
+    stream_type = Column(Enum(VideoStreamTypes))
+    video_box_id = Column(Integer, ForeignKey('video_box.id'))
+    video_box = relationship('VideoBox')
